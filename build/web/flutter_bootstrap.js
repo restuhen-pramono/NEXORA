cf9cf70cfd519a8b3b98e6ccc4c72a64d8ee910f
Restu Hendra Pramono <sipramono24@gmail.com>
2026-06-13 20:58:21 +0700

    fix custom domain

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1794644526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2936228078" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
